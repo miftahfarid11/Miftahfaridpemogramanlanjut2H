c735e0ab4af31cec0bacdee4656eaff6e0b0442a
miftahfarid11 <12050114091@students.uin-suska.ac.id>
2021-03-27 10:58:21 +0700

    miftahfarid

---
+++ src/Array/Arrays.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Arrays {
+    public static void main(String[] args) {
+        double sum = 0.0;
+        for (int x = 0; x< 4; x++) {
+            sum += myArray[x];
+        }
+        System.out.println(sum);
+    }
+}
