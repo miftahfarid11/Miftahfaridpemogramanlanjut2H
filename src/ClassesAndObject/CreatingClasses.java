bf9be502fa49650f7c173c210594878f7249464f
miftahfarid11 <12050114091@students.uin-suska.ac.id>
2021-03-28 22:47:12 +0700

    miftahfarid

---
+++ src/ClassesAndObject/CreatingClasses.java
@@ -0,0 +1,16 @@
+package ClassesAndObject;
+
+import javax.security.sasl.SaslServer;
+
+public class CreatingClasses {
+    public static void main(String[] args) {
+
+    }
+    public static void test(){
+                System.out.println("Hi");
+            }
+
+    }
+
+
+
---
+++ src/ClassesAndObject/MethodParameter.java
@@ -0,0 +1,11 @@
+package ClassesAndObject;
+
+public class MethodParameter {
+    public static void main(String[] args) {
+        doSomething(4);
+    }
+    static void doSomething(int x) {
+        System.out.println(x*x);
+    }
+    }
+
